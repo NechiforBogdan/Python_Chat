efa8c6d11b3918dd93f8eb97425090a6bf8f4f92
NechiforBogdan <71372147+NechiforBogdan@users.noreply.github.com>
2021-11-04 23:45:09 +0100

    Create client_side.py

---
+++ client_side.py
@@ -0,0 +1,29 @@
+import socket
+
+PORT = 585
+FORMAT = 'utf-8'
+HEADER = 64
+DISCONNECTED = "DISCONNECTED"
+SERVER = "192.168.1.23"
+ADDR = (SERVER,PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def sendMessage(msg):
+    message = msg.encode(FORMAT)    # Encodes the message (str) into BYTES format to send it to socket
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length)) # Cuz we need to make the message 64 bytes (header) we need to take the actual message length and substract it from the HEADER (64bytes) 
+    #After subtracting the length of the message from the length of the header, we need to padd it: to make it 64 byes (b" "  = bytes "space" = adds emptry strings (spaces) to make the msg 64bytes)
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+
+sendMessage("Surprise")
+input()
+sendMessage("mother fucker!")
+
+sendMessage(DISCONNECTED)
