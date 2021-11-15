cd1eb1545b3dc4b78aae9da54cfb152c8d4352d1
Pourbaix <71372371+Pourbaix@users.noreply.github.com>
2021-11-15 10:58:05 +0100

    Add files via upload

--- client_side.py
+++ client_side.py
@@ -1,29 +1,47 @@
 import socket
+import threading
 
 PORT = 585
 FORMAT = 'utf-8'
-HEADER = 64
-DISCONNECTED = "DISCONNECTED"
-SERVER = "192.168.1.23"
-ADDR = (SERVER,PORT)
+HEADER = 1024
+SERVER = "192.168.178.20"
+ADDR = (SERVER, PORT)
+
+username = input("Choose a username:")
+print(f"Hello {username} welcome to the chat, if you want to quit this chat just type EXIT")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def sendMessage(msg):
-    message = msg.encode(FORMAT)    # Encodes the message (str) into BYTES format to send it to socket
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length)) # Cuz we need to make the message 64 bytes (header) we need to take the actual message length and substract it from the HEADER (64bytes) 
-    #After subtracting the length of the message from the length of the header, we need to padd it: to make it 64 byes (b" "  = bytes "space" = adds emptry strings (spaces) to make the msg 64bytes)
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+
+def getMessage():
+    while True:
+        try:
+            message = client.recv(HEADER).decode(FORMAT)
+            if message == "USER":
+                client.send(username.encode(FORMAT))
+            else:
+                if username == (message.split(">")[0]):
+                    pass
+                else:
+                    print(message)
+        except:
+            print("Closing connection!\n")
+            client.close()
+            break
 
 
+def sendMessage():
+    while True:
+        inp = input()
+        message = f"{username}>{inp}"
+        client.send(message.encode(FORMAT))
+        if inp == "EXIT":
+            client.close()
+            quit()
 
-sendMessage("Surprise")
-input()
-sendMessage("mother fucker!")
 
-sendMessage(DISCONNECTED)
+receivingTHREAD = threading.Thread(target=getMessage)
+sendingTHREAD = threading.Thread(target=sendMessage)
+receivingTHREAD.start()
+sendingTHREAD.start()
--- server_side.py
+++ server_side.py
@@ -1,8 +1,6 @@
 import socket
 import threading
 
-
-
 """
     Gets IP by host name <socket.gethostbyname>
     Sets port number (choice)
@@ -12,53 +10,88 @@ import threading
         socket() takes 2 param : 
             @AF_IN : Internet Adress family ( AF_INET = IPv4 )
             @SOCK_STREAM : Specifies type of socket ( SOCK_STREAM = TCP  // SOCK_DGRAM = UDP )
-        
-"""
-HEADER = 64
-PORT = 585
-SERVER = socket.gethostbyname(socket.gethostname())  # Gets YOUR IP address (u can type it manually if you want)
-ADDR = (SERVER,PORT)
-FORMAT = "utf-8"
-DISCONNECTED = "DISCONNECTED"
 
-print("Host Name : " + socket.gethostname())  # Can be done by using direcly the public IPv4 of the host 
-print("Host Address : "+ SERVER)
+"""
+HEADER = 1024
+PORT = 585
+SERVER = "10.4.37.148"  # Gets YOUR IP address (u can type it manually if you want)
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+
+users = []
+usernames = []
+
+print("Host Name : " + socket.gethostname())  # Can be done by using direcly the public IPv4 of the host
+print("Host Address : " + SERVER)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creates TCP IPv4 Socket
-server.bind(ADDR)        # Binds IP & PORT 
+server.bind(ADDR)  # Binds IP & PORT
+server.listen()
+print(f"Server listening on : {SERVER}")
 
 
-def client_mungling(connection,address):
-    print(f"New connection : {address}")
+def broadcast(message):
+    for client in users:
+        client.send(message)
+
+
+def handling(client):
     connected = True
     while connected:
-        msg_length = connection.recv(HEADER).decode(FORMAT)     # receiver the size (HEADER) the message must be & decodes it from binary to utf-8
-        if msg_length:                                          # Checks if there is an actual message to send (if somebody has connected)
-            msg_length = int(msg_length)                            # converts to int ( ObViOuSlY  (-_-) )
-            message = connection.recv(msg_length).decode(FORMAT)    # actual message received with the proper length and decoded to uft-8
-            if message == DISCONNECTED:                             # disconnects user (not to cause problems later)
+        try:
+            message = client.recv(HEADER)
+            if message.decode(FORMAT).split(">")[1] == "EXIT":
+                message_disconnected = f"user {message.decode(FORMAT).split('>')[0]} has disconnected."
+                broadcast(message_disconnected.encode(FORMAT))
+                index = 0
+                for i in range(0, len(users)):
+                    if users[i] == client:
+                        index = i
+                        users.remove(users[i])
+                username = usernames[index]
+                usernames.remove(username)
+                print(f"User '{username}' has disconnected!")
                 connected = False
-            print(f"User [{address}] said : {message}")
-            connection.send("Message received".encode(FORMAT))
-    connection.close()                                          
-    
+                break
+            broadcast(message)
+        except:
+            if connected:
+                print("An error has occurred!")
+                break
+            else:
+                pass
+                break
+    client.close()
+    return 0
+
 
 """
     Server listens for any connections until some connects:
         when some1 connects the server stores < server.accept() >:
             store their port & IP in < address > and the information about them (obj) in the < connection >
     !!!server.accept is a "blocker" : stays on same line until connection occurs!!!
-        
+
 """
+
+
 def start():
-    server.listen()
-    print(f"Server listening on : {SERVER}")
     while True:
-        connection,address = server.accept() #Blocks & stocks conn and addr
-        thread = threading.Thread(target=client_mungling, args=(connection,address)) # Creates & sends thread to client_mungling
+        client, address = server.accept()  # accepts all clients onto the server
+        # client =  <socket.socket fd=1244, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=
+        # ('192.168.1.11', 585), raddr=('192.168.1.11', 55547)>
+        # address =  ('192.168.1.11', 55547)
+        client.send("USER".encode(FORMAT))
+        username = client.recv(HEADER).decode(FORMAT)
+        usernames.append(username)
+        print(f"{address} has connected with the username {username}")
+        broadcast(f"{username} has connected.".encode(FORMAT))
+        users.append(client)
+
+        thread = threading.Thread(target=handling, args=(client,))  # Creates & sends thread to client_mungling
         thread.start()
 
-        print(f"Number of active connections : {threading.active_count() - 1}") # -1 because it has to exclude the host
+        print(f"Number of active clients : {threading.active_count() - 1}")  # -1 because it has to exclude the host
+
+
 print("Server is starting...")
 start()
-
