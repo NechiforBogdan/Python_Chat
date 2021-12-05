06d38b0f075600c2af2d11120f799ca3285c1e1c
Pourbaix <71372371+Pourbaix@users.noreply.github.com>
2021-12-05 18:54:53 +0100

    Update dbchat.py

--- dbchat.py
+++ dbchat.py
@@ -1,7 +1,7 @@
-import pymongo
 from pymongo import MongoClient
 import certifi
-
+from datetime import *
+import threading
 
 ca = certifi.where()
 URL = "mongodb+srv://useradmintest:thisisapassword@cluster0.nehju.mongodb.net/Messages?retryWrites=true&w=majority"
@@ -9,9 +9,9 @@ cluster = MongoClient(URL, tlsCAFile=ca)
 db1 = cluster["Chat1"]
 db2 = cluster["Chat2"]
 users1 = db1["Users"]
-message1 = db1["messages"]
+message1 = db1["msg"]
 users2 = db2["Users"]
-message2 = db2["messages"]
+message2 = db2["msg"]
 
 
 def connecting():
@@ -40,12 +40,40 @@ def connecting():
             for user in list_users:
                 if name.split(" ")[0] == user["Name"] and name.split(" ")[1] == user["pwd"]:
                     print(f"Successfully logged with the user {name.split(' ')[0]}!")
-                    return maxId, name.split(" ")[0]
+                    return user["_id"], name.split(" ")[0]
         print("This user doesn't exist or the password is wrong.")
 
 
-def chat(id):
-    pass
+def chat():
+    user = connecting()
+    while True:
+        msg_maxid = 0
+        msg = message1.find()
+        for line in msg:
+            list_users.append(line)
+            msg_maxid = int(line["_id"]) + 1
+        message = input(f"{user[1]}:")
+        now = datetime.now()
+        msg_date = now.strftime("%d/%m/%Y %H:%M")
+        to_send = {"_id": msg_maxid, "user_id": user[0], "msg": message, "date": msg_date}
+        message1.insert_one(to_send)
+
+
+def get_message():
+    list_messages = []
+    for i in message1.find():
+        list_messages.append(i)
+        print(i["msg"])
+    past_len = len(list_messages)
+    while True:
+        list_messages = []
+        for i in message1.find():
+            list_messages.append(i)
+        actual_len = len(list_messages)
+        if actual_len > past_len:
+            for message in list_messages:
+                print(f"{message['msg']}")
+        past_len = actual_len
 
 
 if __name__ == "__main__":
@@ -55,7 +83,8 @@ if __name__ == "__main__":
     for thing in test:
         list_users.append(thing)
         maxId = int(thing["_id"]) + 1
-    print(maxId)
     print(list_users)
-    connection = connecting()
-    chat(connection[0])
+    sendingTHREAD = threading.Thread(target=chat)
+    sendingTHREAD.start()
+    receivingTHREAD = threading.Thread(target=get_message)
+    receivingTHREAD.start()
